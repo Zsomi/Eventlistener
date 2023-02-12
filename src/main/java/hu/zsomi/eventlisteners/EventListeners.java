b18e6363709953b4a97d65ac50ad1b2f2d2d0490
Zsomi <zsomidev@gmail.com>
2023-02-12 11:38:43 +0100

    PlayerBedLeaveEvent megírása.

--- src/main/java/hu/zsomi/eventlisteners/EventListeners.java
+++ src/main/java/hu/zsomi/eventlisteners/EventListeners.java
@@ -1,7 +1,9 @@
 package hu.zsomi.eventlisteners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,12 +14,19 @@ public final class EventListeners extends JavaPlugin implements Listener {
         System.out.println("The plugin loaded!");
 
         getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(this, this);
 
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         e.setJoinMessage("Hi Player!");
+    }
+
+    @EventHandler
+    public void onBedLeaveEvent(PlayerBedLeaveEvent e) {
+        Player player = e.getPlayer();
+        player.setHealth(2);
     }
 
 
